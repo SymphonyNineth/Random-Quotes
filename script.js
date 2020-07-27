c4b68316009f30e6baa78980a089a6f98a371966
Hayk <webdevfix@gmail.com>
2020-07-27 17:35:56 +0400

    spinner has added

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 let quoteField = document.querySelector("#quote"),
-    authorField = document.querySelector("#author");
+    authorField = document.querySelector("#author"),
+    spinner = document.querySelector("#spinner");
 
 fetch("https://type.fit/api/quotes")
   .then(function(response) {
     return response.json();
   })
   .then(function(data) {
-    randomNumber = Math.floor(Math.random()*1643);
+
+    spinner.hidden = true;
+
+    randomNumber = Math.floor(Math.random()*1644);
 
     function getQuote(quotes){
       return quotes[randomNumber].text;
